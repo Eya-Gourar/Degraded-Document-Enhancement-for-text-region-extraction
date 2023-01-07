5ee97c8dd6ad8ee5e8096de0993f804c00130698
eya <gourar.e.y.a@gmail.com>
2023-01-07 19:40:16 +0100

    adding metrics

---
+++ metrics/PSNR.py
@@ -0,0 +1,11 @@
+import numpy as np
+import math
+
+
+
+def psnr(img1, img2):
+    mse = np.mean( (img1 - img2) ** 2 )
+    if (mse == 0):
+        return (100)
+    PIXEL_MAX = 1.0
+    return (20 * math.log10(PIXEL_MAX / math.sqrt(mse)))
